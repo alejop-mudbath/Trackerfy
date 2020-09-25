631fe86b829e821ecb27d7d4d7cab420b74a8bdd
Alejandro Perez <alejandro.perez@innroad.com>
2020-09-25 17:17:46 +1000

    feat: Create new issue command request validator

---
+++ src/Trackerfy.Application/Commands/CreateIssue/CreateIssueCommandValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace Trackerfy.Application.Commands.CreateIssue
+{
+    public class CreateIssueCommandValidator: AbstractValidator<CreateIssueCommand>
+    {
+        public CreateIssueCommandValidator()
+        {
+            RuleFor(v => v.Summary)
+                .NotEmpty().WithMessage("Summary is required.")
+                .MaximumLength(100).WithMessage("Summary must not exceed 200 characters.");
+
+            RuleFor(v => v.IssueTypeId)
+                .NotEmpty().WithMessage("Issue Type Id is required.");
+        }
+    }
+}
---
+++ src/Trackerfy.IntegrationTests/CreateIssueCommandValidatorTests.cs
@@ -0,0 +1,43 @@
+using Shouldly;
+using Trackerfy.Application;
+using Trackerfy.Application.Commands.CreateIssue;
+using Xunit;
+
+namespace Trackerfy.IntegrationTests
+{
+    public class CreateIssueCommandValidatorTests
+    {
+        [Fact]
+        public void ShouldBeTrue_WhenSummaryIsNotEmpty()
+        {
+            var command = new CreateIssueCommand("summary", 21);
+
+            var validator = new CreateIssueCommandValidator();
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void ShouldBeFalse_WhenSummaryIsEmpty()
+        {
+            var command = new CreateIssueCommand("", 21);
+
+            var validator = new CreateIssueCommandValidator();
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBe(false);
+        }
+
+        [Fact]
+        public void ShouldBeFalse_WheIssueTypeIsEmpty()
+        {
+            var command = new CreateIssueCommand("summary", 0);
+
+            var validator = new CreateIssueCommandValidator();
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBe(false);
+        }
+    }
+}
