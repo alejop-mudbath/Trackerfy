63bf386544d4719bbdcecbfca282b739cec6d62e
Alejandro Perez <alejandro.perez@innroad.com>
2020-09-25 18:37:00 +1000

    feat: SqlLite db configuration

---
+++ src/Trackerfy.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Trackerfy.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<Context>(options =>
+                options.UseSqlite("Data Source=trackerfy.db",
+                    b => b.MigrationsAssembly(typeof(Context).Assembly.FullName)));
+
+            services.AddScoped<IContext>(provider => provider.GetService<Context>());
+
+            return services;
+        }
+    }
+}
