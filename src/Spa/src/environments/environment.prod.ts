67308b61a7a764e15d27ff2bb0819209857df63b
Alejandro Perez <alejandro.perez@innroad.com>
2020-10-02 13:29:32 +1000

    build: azure angular build

--- src/Spa/src/environments/environment.prod.ts
+++ src/Spa/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:5000/api',
+  auth: {
+    domain: 'trackerfy.us.auth0.com',
+    clientId: 'PJhl8w04c6MrpWfmwOifNzkYPDi9EarR',
+    audience: 'https://trackerfy-api.azurewebsites.net/',
+    redirect: 'https://trackerfy.azurewebsites.net/callback',
+    appUrl: 'https://trackerfy.azurewebsites.net',
+    scope: 'openid profile email'
+  }
 };
