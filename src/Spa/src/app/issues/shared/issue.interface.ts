c48b99ded2ad3c9a044f012c8ad66047f2102b8e
Alejandro Perez <alejandro.perez@innroad.com>
2020-10-02 21:50:57 +1000

    feat: issue create and modification details

--- src/Spa/src/app/issues/shared/issue.interface.ts
+++ src/Spa/src/app/issues/shared/issue.interface.ts
@@ -1,12 +1,14 @@
 export interface IssueInterface {
-  "id": number,
-  "summary": string,
-  "issueTypeId": number,
-  "issueTypeDescription": string,
-  "issueStateId": number,
-  "issueStateDescription": string,
-  "createdBy": string,
-  "created": Date,
-  "lastModifiedBy": string,
-  "lastModified": Date,
+  'id': number;
+  'summary': string;
+  'issueTypeId': number;
+  'issueTypeDescription': string;
+  'issueStateId': number;
+  'issueStateDescription': string;
+  'createdBy': string;
+  'created': Date;
+  'lastModifiedBy': string;
+  'lastModified': Date;
+  lastModifiedByName: string;
+  createdByName: string;
 }
